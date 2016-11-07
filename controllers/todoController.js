6ac20572af975a14798a6c089c5046c35f98a872
dawn_scroll <dibyomails007gmail.com>
2016-11-08 03:38:51 +0530

    controller ready

---
+++ controllers/todoController.js
@@ -0,0 +1,14 @@
+module.exports = function(app){
+
+  app.get('/todo', function(req, res){
+
+  });
+
+  app.get('/todo', function(req, res){
+
+  });
+
+  app.delete('/todo', function (req, res) {
+    
+  });
+};
